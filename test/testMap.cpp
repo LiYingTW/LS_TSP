8be0597b431fdc3cc907831c914459888595a581
Li-Yng <ly101u@cs.ccu.edu.tw>
2016-04-08 17:27:11 +0800

    read from eil51.tsp

--- test/testMap.cpp
+++ test/testMap.cpp
@@ -9,13 +9,13 @@
 using namespace std;
 int main()
 {
-	// ***	test 001	***
+	/***	test 001	***
 	int numOfTown = 10;
 	string fileName = "test";
 
 	Map* map = new Map(numOfTown, fileName);
+	*/
 
-
-//	Map* map = new Map(51, "./data/eil51.tsp");
-//	return 0;
+	Map* map = new Map(51, "./data/eil51.tsp");
+	return 0;
 }
